ca9e132a9fb1bb3fe11c801ed07a7233b537c069
PatBarnabe <patbarnabe5@gmail.com>
2024-05-14 01:23:56 -0300

    Catching params from routing

--- app/country/[name]/page.tsx
+++ app/country/[name]/page.tsx
@@ -1,3 +1,7 @@
-export default function Country() {
-  return <h1>Country</h1>;
+export default function Country({
+  params: { name }, //Está relacionado ao [name] que é o nome da pasta onde esse arquivo está (o parâmetro deve ter o mesmo nome da pasta)
+}: {
+  params: { name: string }; //Criação de tipo para o parâmetro que vem do path da rota da URL
+}) {
+  return <h1>{name}</h1>;
 }
