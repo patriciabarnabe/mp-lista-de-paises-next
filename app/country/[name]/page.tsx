dad6580bf85f2d1fdb953f50b387d70e55a79930
patriciabarnabe <patbarnabe5@gmail.com>
2024-05-15 18:20:22 -0300

    Details in flag layout

--- app/country/[name]/page.tsx
+++ app/country/[name]/page.tsx
@@ -101,12 +101,12 @@ export default async function CountryDetail({
             </h2>
           )}
         </section>
-        <div className="relative h-48 my-2 md:h-auto w-96 shadow-md rounded-xl md:order-last order-first mx-auto md:mx-0">
+        <div className="relative h-48 my-2 md:h-auto w-96 shadow-md md:order-last order-first rounded-xl">
           <Image
             src={country.flags.svg}
             alt={country.flags.alt}
             fill
-            className="object-cover rounded-xl w-full h-full"
+            className="object-cover rounded-xl"
           />
         </div>
       </article>
