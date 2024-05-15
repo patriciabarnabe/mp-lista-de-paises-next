30b1f95b632f3ef49bedbc70eeff0621385e11c9
patriciabarnabe <patbarnabe5@gmail.com>
2024-05-15 17:51:00 -0300

    Adding go back to page nav title

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Nunito_Sans } from "next/font/google";
 import "./globals.css";
 import Image from "next/image";
+import Link from "next/link";
 
 const nunitoSans = Nunito_Sans({ subsets: ["latin"] });
 
@@ -21,14 +22,16 @@ export default function RootLayout({
       <body className={nunitoSans.className}>
         <main className="bg-gray-100 min-h-screen flex flex-col items-center">
           <nav className="bg-white w-full h-16 flex items-center justify-center">
-            <section className="container flex items-center gap-3">
-              <Image
-                src="/logo.svg"
-                alt="Logo da aplicação - emoji de globo"
-                width={48}
-                height={48}
-              />
-              <h1 className="font-bold text-2xl">Países do Mundo</h1>
+            <section className="container">
+              <Link href="/" className="flex items-center py-2 gap-3">
+                <Image
+                  src="/logo.svg"
+                  alt="Logo da aplicação - emoji de globo"
+                  width={48}
+                  height={48}
+                />
+                <h1 className="font-bold text-2xl">Países do Mundo</h1>
+              </Link>
             </section>
           </nav>
           {children}{" "}
